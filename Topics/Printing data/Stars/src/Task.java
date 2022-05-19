117eadea0416b5c687fb8013fc1d8a50e0606e1b
LIGHT 1111 <JOEJOHNSON0204@outlook.com>
2022-05-19 03:43:42 -0400

    Creation of new Blockchain Course

---
+++ Topics/Printing data/Stars/src/Task.java
@@ -0,0 +1,20 @@
+// You can experiment here, it won’t be checked
+
+public class Task {
+  public static void main(String[] args) {
+    // put your code here
+    // What does the following code print?
+
+    System.out.print("**");
+    System.out.println();
+    System.out.println("**");
+    System.out.print("**");
+  }
+
+  /*
+   **
+
+   **
+   **
+   */
+}
